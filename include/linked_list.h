a07326954e060363dd9c6e60ff1c89f064c98573
Jonathan McAndrew <jonmc2358@gmail.com>
2026-03-03 20:22:11 -0500

    Implement CLI for linked list
    Also, improved some of the linked list functions

--- include/linked_list.h
+++ include/linked_list.h
@@ -9,14 +9,39 @@ typedef struct ll_item {
 /**
  * Returns the last item in the list, or NULL if the list is empty.
  */
-ll_item_t* get_last_item(ll_item_t* list_head);
+ll_item_t* ll_get_last_item(ll_item_t* list_head);
+
+/**
+ * Returns the item at the given index in the list, or NULL if the index is out of bounds
+ */
+ll_item_t* ll_get_item_at_index(ll_item_t* list_head, int index);
+
+/**
+ * Prepends a new item with the given value to the beginning of the list
+ */
+ll_item_t* ll_insert_at_start(ll_item_t* list_head, int value);
 
 /**
  * Appends a new item with the given value to the end of the list
  */
-ll_item_t* append_item(ll_item_t* list_head, int value);
+ll_item_t* ll_insert_at_end(ll_item_t* list_head, int value);
+
+/**
+ * Inserts a new item with the given value at the specified index in the list
+ */
+ll_item_t* ll_insert_at_index(ll_item_t* list_head, int index, int value);
+
+/**
+ * Traverse the linked list and apply the given function to each value
+ */
+void ll_traverse(ll_item_t* list_head, void (*func)(int));
 
 /**
  * Print the list
  */
-void print_list(ll_item_t* list_head);
+void ll_print(ll_item_t* list_head);
+
+/**
+ * Get the length of the list
+ */
+int ll_get_length(ll_item_t* list_head);
--- src/linked_list.c
+++ src/linked_list.c
@@ -1,16 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "../include/linked_list.h"
 
 int main(int argc, char *argv[]) {
 	ll_item_t* list_head = NULL;
-	for (int index = 0; index < 10; index++) {
-		list_head = append_item(list_head, index);
+	char op[32];
+	int arg1;
+	int arg2;
+	int arg3;
+
+	const char help_message[] = "Operations:"
+		"\n- print"
+		"\n- length"
+		"\n- get <index>"
+		"\n- prepend <value>"
+		"\n- append <value>"
+		"\n- insert <index> <value>"
+		"\n- append-random-values <count> <min> <max>"
+		"\n- help"
+		"\n- exit|quit\n";
+
+	printf(help_message);
+	while (1) {
+		printf("\n> ");
+		scanf("%s", op);
+		if (strcmp(op, "print") == 0) {
+			ll_print(list_head);
+		} else if (strcmp(op, "length") == 0) {
+			printf("Length: %d\n", ll_get_length(list_head));
+		} else if (strcmp(op, "get") == 0) {
+			scanf("%d", &arg1);
+			ll_item_t* item = ll_get_item_at_index(list_head, arg1);
+			if (item != NULL) {
+				printf("Value at index %d is %d\n", arg1, item->value);
+			} else {
+				printf("Index %d is out of bounds\n", arg1);
+			}
+		} else if (strcmp(op, "prepend") == 0) {
+			scanf("%d", &arg1);
+			list_head = ll_insert_at_start(list_head, arg1);
+			printf("Prepended %d to the start of the list\n", arg1);
+		} else if (strcmp(op, "append") == 0) {
+			scanf("%d", &arg1);
+			list_head = ll_insert_at_end(list_head, arg1);
+			printf("Appended %d to the end of the list\n", arg1);
+		} else if (strcmp(op, "insert") == 0) {
+			scanf("%d %d", &arg1, &arg2);
+			list_head = ll_insert_at_index(list_head, arg1, arg2);
+			printf("Inserted %d at index %d\n", arg2, arg1);
+		} else if (strcmp(op, "append-random-values") == 0) {
+			scanf("%d %d %d", &arg1, &arg2, &arg3);
+			srand(time(NULL));
+			for (int i = 0; i < arg1; i++) {
+				int random_int = (rand() % (arg3 - arg2 + 1)) + arg2;
+				list_head = ll_insert_at_end(list_head, random_int);
+			}
+			printf("Appended %d random values between %d and %d\n", arg1, arg2, arg3);
+		} else if (strcmp(op, "help") == 0) {
+			printf(help_message);
+		} else if (strcmp(op, "exit") == 0 || strcmp(op, "quit") == 0) {
+			printf("\nExiting...\n");
+			break;
+		} else {
+			printf("\nUnknown operation: %s\n\n", op);
+			printf(help_message);
+		}
 	}
-	print_list(list_head);
+	return 0;
 }
 
-ll_item_t* get_last_item(ll_item_t* list_head) {
+ll_item_t* ll_get_last_item(ll_item_t* list_head) {
 	if (list_head == NULL) {
 		return NULL;
 	}
@@ -21,35 +82,89 @@ ll_item_t* get_last_item(ll_item_t* list_head) {
 	return list_item;
 }
 
-ll_item_t* append_item(ll_item_t* list_head, int value) {
+ll_item_t* ll_get_item_at_index(ll_item_t* list_head, int index) {
+	int current_index = 0;
+	ll_item_t* list_item = list_head;
+	while (list_item != NULL) {
+		if (current_index == index) {
+			return list_item;
+		}
+		current_index++;
+		list_item = list_item->next;
+	}
+	return NULL; // Index out of bounds
+}
+
+ll_item_t* ll_insert_at_start(ll_item_t* list_head, int value) {
+	ll_item_t* new_item = malloc(sizeof(ll_item_t));
+	new_item->value = value;
+	new_item->next = list_head;
+	return new_item;
+}
+
+ll_item_t* ll_insert_at_end(ll_item_t* list_head, int value) {
 	ll_item_t* new_item = malloc(sizeof(ll_item_t));
 	new_item->value = value;
 	new_item->next = NULL;
-	printf("Appending Value: %d\n", new_item->value);
-
 	if (list_head == NULL) {
 		return new_item;
 	}
-
-	ll_item_t* last_item = get_last_item(list_head);
+	ll_item_t* last_item = ll_get_last_item(list_head);
 	last_item->next = new_item;
 	return list_head;
 }
 
-void print_list(ll_item_t* list_head) {
-	printf("\n\nprint_list() ...\n");
-	if (list_head == NULL) {
-		return;
+ll_item_t* ll_insert_at_index(ll_item_t* list_head, int index, int value) {
+	if (index == 0) {
+		return ll_insert_at_start(list_head, value);
 	}
+	int current_index = 0;
 	ll_item_t* list_item = list_head;
-
-	while (1) {
-		printf("%d", list_item->value);
-		if (list_item->next == NULL) {
+	while (list_item != NULL) {
+		if (current_index == index - 1) {
+			ll_item_t* new_item = malloc(sizeof(ll_item_t));
+			new_item->value = value;
+			new_item->next = list_item->next;
+			list_item->next = new_item;
 			break;
 		}
-		printf(" -> ");
+		current_index++;
 		list_item = list_item->next;
 	}
-	printf("\n\n");
+	return list_head; // No change to the list
+}
+
+void ll_traverse(ll_item_t* list_head, void (*func)(int)) {
+	ll_item_t* list_item = list_head;
+	while (list_item != NULL) {
+		func(list_item->value);
+		list_item = list_item->next;
+	}
+}
+
+/**
+ * Helper function to print a value, used in ll_print
+ */
+void print_value(int value) {
+	printf("%d, ", value);
+}
+
+void ll_print(ll_item_t* list_head) {
+	if (list_head == NULL) {
+		printf("\nThe list is empty.\n");
+		return;
+	}
+	printf("\nValues: \n");
+	ll_traverse(list_head, print_value);
+	printf("\n");
+}
+
+int ll_get_length(ll_item_t* list_head) {
+	int length = 0;
+	ll_item_t* list_item = list_head;
+	while (list_item != NULL) {
+		length++;
+		list_item = list_item->next;
+	}
+	return length;
 }
